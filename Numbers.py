4ff5f9943d3ca62b00048b6380230fe16d94fb0d
Dan Kalashian <dkalashi@terpmail.umd.edu>
2023-12-08 09:57:41 -0500

    First commit

---
+++ Numbers.py
@@ -0,0 +1,17 @@
+##NUMPY and pandas
+import numpy as np
+import pandas as pd
+x= np.array ([[1.,2.,3.],
+            [10.,100.,100.]])
+print(x)
+print(x*2)
+
+flowers = pd.read_csv("iris_data.csv")
+print(flowers[["Species", "obs", "flower_part"]])
+
+Dans_Thoughts = {'Activity': ['book', 'tv', 'games', 'studying'],
+        'Days Since Last time': [1, 0, 11, 0],
+        'Enjoyable': ['Yes', 'Yes', 'Yes', 'No']}
+df = pd.DataFrame(Dans_Thoughts)
+
+df.to_csv('Dans_Thoughts.csv', index=False)
