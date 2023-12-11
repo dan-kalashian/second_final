0719c51d7e095a39106d140e8f0d60f7dfa6053a
Dan Kalashian <dkalashi@terpmail.umd.edu>
2023-12-11 18:52:36 -0500

    Added comments and readme

--- Numbers.py
+++ Numbers.py
@@ -1,19 +1,30 @@
-##NUMPY and pandas
+#numpy and pandas
+#imports
 import numpy as np
 import pandas as pd
+
+#created an array using numpy
 x= np.array ([[1.,2.,3.],
             [10.,100.,100.]])
+
+#printed array normally then as modified to have each value doubled
 print(x)
 print(x*2)
 
+#read csv using pandas
 flowers = pd.read_csv("iris_data.csv")
+#printed data frame for chosen variables
 print(flowers[["Species", "obs", "flower_part"]])
+#using panda objects for data analysis
 print(flowers.describe())
+#print data frame for data where the length variable has a value greater than five
 print(flowers[(flowers["Length"]>5)])
 
+#create a dictionary with a few activities, how recently I did them, and if I thought is was enjoyable
 Dans_Thoughts = {'Activity': ['book', 'tv', 'games', 'studying'],
         'Days Since Last time': [1, 0, 11, 0],
         'Enjoyable': ['Yes', 'Yes', 'Yes', 'No']}
+#used pandas to make this dictionary a data frame
 df = pd.DataFrame(Dans_Thoughts)
-
+#wrote this data frame as a CSV file
 df.to_csv('Dans_Thoughts.csv', index=False)
---
+++ README.md
@@ -0,0 +1,8 @@
+This is the README file for my second final project.
+This project has no user inputs
+
+The numbers.py file creates an array then prints that array, then prints that array with values doubled.
+The program then works with the iris data using pandas, and prints various data frames
+The program then takes a dictionary, converts it to a pandas data frame, and exports that as a CSV
+
+Thank you for reading!
